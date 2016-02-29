f14a5322a7118e1ec15df4969f1b6738b7e2a3fe
samsax <samuelsaxofon@gmail.com>
2016-02-29 18:54:10 -0500

    ...

--- app/src/main/java/co/edu/udea/compumovil/gr12/lab1ui/MainActivity.java
+++ app/src/main/java/co/edu/udea/compumovil/gr12/lab1ui/MainActivity.java
@@ -1,30 +1,33 @@
 package co.edu.udea.compumovil.gr12.lab1ui;
 
 import android.os.Bundle;
-import android.support.annotation.StringDef;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText nombre, apellido, pais, telefono, direccion, email;
+    AutoCompleteTextView ac_paises;
     RadioGroup sexo;
     Spinner hobbies;
     CheckBox favorito;
     TextView texto;
     Button boton;
+    DatePicker dp_fecha;
+    Date fecha;
 
 
     @Override
@@ -39,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         sexo = (RadioGroup) findViewById(R.id.Sexo);
 
+        dp_fecha = (DatePicker) findViewById(R.id.fecha);
         pais = (EditText) findViewById(R.id.pais);
         telefono = (EditText) findViewById(R.id.telefono);
         direccion = (EditText) findViewById(R.id.direccion);
@@ -46,15 +50,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         hobbies = (Spinner) findViewById(R.id.hobbies);
         favorito = (CheckBox) findViewById(R.id.favorito);
+        ac_paises = (AutoCompleteTextView) findViewById(R.id.pais);
 
         texto = (TextView) findViewById(R.id.datos);
         boton = (Button) findViewById(R.id.mostrar);
 
+        String[] paises  = getResources().getStringArray(R.array.paises);
+
         boton.setOnClickListener(this);
 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.hobbies, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         hobbies.setAdapter(adapter);
+
+        ArrayAdapter<String> adapterPaises = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, paises);
+        ac_paises.setAdapter(adapterPaises);
+        ac_paises.setThreshold(1);
+
     }
 
     @Override
@@ -82,19 +94,73 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if(v.getId()==R.id.mostrar){
-            String date = "Nombre: " + nombre.getText().toString() + "\n";
-            date = date +"Apellido: "+ apellido.getText().toString() + "\n";
+            String genero = null;
+            int sex = sexo.getCheckedRadioButtonId();
+            switch (sex) {
+                case R.id.Masculino:
+                    genero = getString(R.string.masculino);
+                    break;
+                case R.id.Femenino:
+                    genero = getString(R.string.femenino);
+                    break;
 
-            date = date +"Pais: "+ pais.getText().toString() + "\n";
-            date = date +"Telefono: "+ telefono.getText().toString() + "\n";
-            date = date +"Dirección: "+ direccion.getText().toString() + "\n";
-            date = date +"E-mail: "+ email.getText().toString() + "\n";
-            if(favorito.isChecked()){
-                date = date + "Contacto marcado como favorito";
-            }else{
-                date = date + "Contacto No marcado como favorito";
             }
-            texto.setText(date);
+
+            String hobbie;
+            int hobby = hobbies.getSelectedItemPosition();
+            hobbie = hobbies.getItemAtPosition(hobby).toString();
+            String mostrarTodo="";
+
+            if(nombre.getText().toString().isEmpty()){
+                mostrarTodo=getString(R.string.faltan_nombre);
+                texto.setText(mostrarTodo);
+                return;
+            }
+            if(apellido.getText().toString().isEmpty()){
+                mostrarTodo=getString(R.string.faltan_apellido);
+                texto.setText(mostrarTodo);
+                return;
+            }
+            if(pais.getText().toString().isEmpty()){
+                mostrarTodo=getString(R.string.faltan_pais);
+                texto.setText(mostrarTodo);
+                return;
+            }
+            if(telefono.getText().toString().isEmpty()){
+                mostrarTodo=getString(R.string.faltan_telefono);
+                texto.setText(mostrarTodo);
+                return;
+            }
+            if(direccion.getText().toString().isEmpty()){
+                mostrarTodo=getString(R.string.faltan_direccion);
+                texto.setText(mostrarTodo);
+                return;
+            }
+            if(email.getText().toString().isEmpty()){
+                mostrarTodo=getString(R.string.faltan_email);
+                texto.setText(mostrarTodo);
+                return;
+            }
+
+            mostrarTodo = mostrarTodo +getString(R.string.nombre)+" "+ nombre.getText().toString() + "\n";
+            mostrarTodo = mostrarTodo +getString(R.string.apellido)+" "+ apellido.getText().toString() + "\n";
+            mostrarTodo = mostrarTodo +getString(R.string.sexo)+" "+ genero + "\n";
+            mostrarTodo = mostrarTodo +getString(R.string.pais)+" "+ pais.getText().toString() + "\n";
+
+            String day= Integer.toString(dp_fecha.getDayOfMonth());
+            String month=Integer.toString(dp_fecha.getMonth()+1);
+            String year=Integer.toString(dp_fecha.getYear());
+            mostrarTodo = mostrarTodo +getString(R.string.fecha)+" "+ day +"/"+month+"/"+year + "\n";
+            mostrarTodo = mostrarTodo +getString(R.string.telefono)+" "+ telefono.getText().toString() + "\n";
+            mostrarTodo = mostrarTodo +getString(R.string.direccion)+" "+ direccion.getText().toString() + "\n";
+            mostrarTodo = mostrarTodo +getString(R.string.email)+" "+ email.getText().toString() + "\n";
+            mostrarTodo = mostrarTodo +getString(R.string.hobby)+" "+ hobbie + "\n";
+            if(favorito.isChecked()){
+                mostrarTodo = mostrarTodo + getString(R.string.si_favorito);
+            }else{
+                mostrarTodo = mostrarTodo + getString(R.string.no_favorito);
+            }
+            texto.setText(mostrarTodo);
         }
     }
 }
